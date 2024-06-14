9454a45bfaa787e07c1672d6c88089538e0e50d1
Bruno Balduccio <bruno.baldu@akalab.tech>
2024-06-13 21:51:10 -0300

    Se agrega clase Producto

---
+++ Dominio/Producto.cs
@@ -0,0 +1,9 @@
+namespace Dominio;
+
+public class Producto
+{
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public double Precio { get; set; }
+    public int Stock { get; set; }
+}
