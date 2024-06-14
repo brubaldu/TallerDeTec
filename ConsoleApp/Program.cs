9ba3c416142ad064d3bc4830489e162648129b56
kzSanto <santiago.andreoli@kzsoftworks.com>
2024-06-13 22:22:07 -0300

    Se agrega menu principal con sus opciones

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    string opcion;
+    Console.WriteLine("1- Registrar producto");
+    Console.WriteLine("2- Comprar producto");
+    opcion = Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+    switch (opcion)
+    {
+        case "1":
+
+            break;
+
+        case "2":
+
+            break;
+
+        default:
+            break;
+    }
+}
